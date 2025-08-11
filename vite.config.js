23ebe406960c1c80c5aedacf279a86515c76b2c9
dony102 <donyespapa@gmail.com>
2025-08-11 11:07:03 +0700

    fix: set base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,3 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Ganti 'veo3-react' dengan nama repo kamu
 export default defineConfig({
   base: '/veo3-react/',
   plugins: [react()],
